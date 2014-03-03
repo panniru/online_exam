f1bfc4a29999d0ea3cdad28a8d5d5e52b066576d
Srikanth <sri.panniru08@gmail.com>
2014-03-03 11:33:31 +0530

    rename file name change

--- db/migrate/20140303053921_rename_semister_to_semester.rb
+++ db/migrate/20140303053921_rename_semister_to_semester.rb
@@ -1,4 +1,4 @@
-class RenameSemesterToSemester < ActiveRecord::Migration
+class RenameSemisterToSemester < ActiveRecord::Migration
   def change
     rename_column :exams, :semister, :semester
     rename_column :students, :semister, :semester
