f3d1a287dbafcaa029a4d7b5cab3697bc314d164
Srikanth <sri.panniru08@gmail.com>
2014-03-12 19:27:38 +0530

    static page temporary change

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   skip_authorization_check
 
   def index
-    if current_user.faculty?
+    if current_user.present? and current_user.faculty?
       page = params[:page].present? ? params[:page] : 1
       @courses = current_user.resource.courses.paginate(:page => page)
       render "courses/index"
