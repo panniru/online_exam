ac1ad9763076e3c94ce08e6551841ea61cd3e5ae
Srikanth <sri.panniru08@gmail.com>
2014-03-26 16:43:13 +0530

    question id removed

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,7 +10,6 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'srikanth@ostryalabs.com'
 
   # Configure the class responsible to send e-mails.
   #config.mailer = UserMailer
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-OnlineExam::Application.config.session_store :cookie_store, key: '_online_exam_session'
+OnlineExam::Application.config.session_store :active_record_store #:cookie_store, key: '_online_exam_session'
