ddfb876e5623c56369c547470367008759382f98
Srikanth <sri.panniru08@gmail.com>
2015-01-14 21:44:53 +0530

    envornment.rb changed

--- Gemfile
+++ Gemfile
@@ -64,12 +64,12 @@ group :development, :test do
 end
 gem 'factory_girl_rails', '~> 4.0'
 gem 'delayed_job_active_record'
-gem 'unicorn', '~> 4.8.3'
+gem 'unicorn'
 
 group :development do
   gem 'capistrano', '~> 3.3.0'
   gem 'capistrano-rbenv', '~> 2.0'
   gem 'capistrano-bundler', '~> 1.1.2'
   gem 'capistrano-rails', '~> 1.1'
-  gem 'capistrano-unicorn-nginx', '~> 3.1.0'
+  #gem 'capistrano-unicorn-nginx', '~> 3.1.0'
 end
--- config/environment.rb
+++ config/environment.rb
@@ -4,9 +4,9 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 OnlineExam::Application.initialize!
 
-Date::DATE_FORMATS[:default] = lambda { |date| I18n.l(date) }
+#Date::DATE_FORMATS[:default] = lambda { |date| I18n.l(date) }
 Time::DATE_FORMATS[:default] = "%d/%m/%Y %H:%M:%S"
 
-Rails.logger = Logger.new(STDOUT)
-Rails.logger.level = 0
+# Rails.logger = Logger.new(STDOUT)
+# Rails.logger.level = 0
 
