717ff53017e496a84625544b642fdee11599acbe
Srikanth <sri.panniru08@gmail.com>
2015-03-25 18:30:56 +0530

    fix: sq query fix in student index search

--- app/controllers/auto_search_controller.rb
+++ app/controllers/auto_search_controller.rb
@@ -12,7 +12,10 @@ class AutoSearchController < ApplicationController
   autocomplete :exam, :exam_name, :full => true
 
   def autocomplete_student_roll_number
-    students = Student.where("(lower(roll_number) ILIKE '%#{params[:term]}%') AND (id in (SELECT student_id from results where schedule_id in (SELECT id from schedules where exam_id = #{params[:exam_id]} AND schedule_date = '#{params[:schedule_date]}')))")
+    where_clause = nil
+    where_clause = "where exam_id = #{params[:exam_id]} AND schedule_date = '#{params[:schedule_date]}'" if params[:exam_id].present? and params[:schedule_date].present?
+
+    students = Student.where("(lower(roll_number) ILIKE '%#{params[:term]}%') AND (id in (SELECT student_id from results where schedule_id in (SELECT id from schedules #{where_clause if where_clause.present?})))")
     render :json => students.map{ |student| {:id => student.id, :label => student.roll_number}}
   end
 
