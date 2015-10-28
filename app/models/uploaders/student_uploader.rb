0709bc85683bb213c4f375081c5ba91c3ce4edbc
Srikanth <sri.panniru08@gmail.com>
2015-10-28 10:30:11 +0530

    student rollno rounded to integer

--- app/models/uploaders/student_uploader.rb
+++ app/models/uploaders/student_uploader.rb
@@ -19,13 +19,14 @@ class StudentUploader
       student = Student.find_by_id(row["id"]) || Student.new
       student.attributes = row.to_hash.slice("name", "year", "semester", "roll_number")
       if row["course"].present?
-        student.course_id = Course.where("lower(name) = ?", row["course"].downcase).first.try(:id)
+        student.course_id = Course.where("lower(name) = ?", row["course"].downcase.strip).first.try(:id)
       else
         student.course_id = course_id
       end
       student.academic_year = Course.current_academic_year
       user = student.build_user
       user.attributes = {:email => row["email"], :user_id => student.roll_number.to_s, :password => "welcome", :password_confirmation => "welcome", :role_id => Role.student_role.id }
+      student.roll_number = student.roll_number.to_i.to_s
       student
     end
   end
