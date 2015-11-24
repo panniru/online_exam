b9354e8d9195cd56f7da8bdd817825967f43884b
krrish <santosh.28@hotmail.com>
2015-11-24 13:11:58 +0530

    issue resolved, saving user by rollnumber

--- app/models/uploaders/student_uploader.rb
+++ app/models/uploaders/student_uploader.rb
@@ -16,7 +16,7 @@ class StudentUploader
 
   def save
     super do |row|
-      student = Student.find_by_id(row["id"]) || Student.new
+      student = Student.find_by_roll_number(row["roll_number"].to_i.to_s) || Student.new
       student.attributes = row.to_hash.slice("name", "year", "semester", "roll_number")
       if row["course"].present?
         student.course_id = Course.where("lower(name) = ?", row["course"].downcase.strip).first.try(:id)
@@ -24,8 +24,12 @@ class StudentUploader
         student.course_id = course_id
       end
       student.academic_year = Course.current_academic_year
+      existed_user = User.find_by_user_id(row["roll_number"].to_s) || User.find_by_user_id(row["roll_number"].to_i.to_s) 
+      if(existed_user.present?)
+        existed_user.destroy
+      end
       user = student.build_user
-      user.attributes = {:email => row["email"], :user_id => student.roll_number.to_s, :password => "welcome", :password_confirmation => "welcome", :role_id => Role.student_role.id }
+      user.attributes = {:email => row["email"], :user_id => student.roll_number.to_i.to_s, :password => "welcome", :password_confirmation => "welcome", :role_id => Role.student_role.id }
       student.roll_number = student.roll_number.to_i.to_s
       student
     end
